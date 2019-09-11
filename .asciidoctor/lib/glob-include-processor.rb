f2115a0e7458a872a38f6594965a9b4b2654ad65
Alexander Schwartz <alexander.schwartz@gmx.net>
2019-09-11 12:04:44 +0200

    formatting

--- .asciidoctor/lib/glob-include-processor.rb
+++ .asciidoctor/lib/glob-include-processor.rb
@@ -1,3 +1,6 @@
+# Taken from the AsciiDoctor extensions lab
+# https://github.com/asciidoctor/asciidoctor-extensions-lab
+
 RUBY_ENGINE == 'opal' ? (require 'glob-include-processor/extension') : (require_relative 'glob-include-processor/extension')
 
 Asciidoctor::Extensions.register do
